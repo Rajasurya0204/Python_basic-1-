3ac58d1c813040782e457bea01174d865819ef95
Rajasurya0204 <47408513+Rajasurya0204@users.noreply.github.com>
2019-02-08 12:31:07 +0530

    Create febi.py

---
+++ febi.py
@@ -0,0 +1,15 @@
+nterms = int(input())
+n1 = 0
+n2 = 1
+count = 0
+if nterms == 1:
+   print(nterms)
+   print(n1)
+else:
+   print(nterms)
+   while count < nterms:
+       print(n1,end=" ")
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
